977346781b06501a6f354af022b54c10622b8e3d
glenpassow <glenpassow@hanovernorwichschools.org>
2016-02-23 14:56:48 -0500

    Updated from Brython Server: February 23, 2016 2:56:48 PM EST

--- stringjumble.py
+++ stringjumble.py
@@ -26,4 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 words = str(input("Please enter a string of text (the bigger the better): "))
+characters = len(words)
+
 print(words)
+print(characters)
