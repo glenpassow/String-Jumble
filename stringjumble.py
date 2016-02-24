9f4ccb22115b81f854f596558bdabc2fbda69ee7
glenpassow <glenpassow@hanovernorwichschools.org>
2016-02-23 19:12:59 -0500

    Updated from Brython Server: 2/23/2016 7:12:56 PM

--- stringjumble.py
+++ stringjumble.py
@@ -35,7 +35,6 @@ flip2 = words .split(' ')[::-1]
 flip2 = " ".join(str(x) for x in flip2)
 print(flip2)
 
-#flip3 = words[::-1]
 flip3 = flip1 .split(' ')[::-1]
 flip3 = " ".join(str(x) for x in flip3)
 print(flip3)
